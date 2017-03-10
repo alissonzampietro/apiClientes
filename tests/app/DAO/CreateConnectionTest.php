0908ec1a70ca384161e70e14ce03e25d0248c595
Alisson Zampietro <alisson.zampietro@gmail.com>
2017-03-10 19:07:53 -0300

    Criando DAO

--- tests/app/DAO/CreateConnectionTest.php
+++ tests/app/DAO/CreateConnectionTest.php
@@ -5,10 +5,12 @@ namespace App\DAO\;
 class CreateConnection {
 	private static $database = NULL;
 
-	public function __construct() {
-
+	public function __construct($obgDatabase) {
+		$self::database = $obgDatabase;
 	}
 	public static getConnection() {
+		if($self::database == null)
+			throw new Exception("Não foi uma criada conexão!")
 		return $self::database;
 	}
 }
